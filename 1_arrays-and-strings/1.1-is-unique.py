5e67c4b8b13cf8da1b7185d0fadf8371ac85b990
jayellho <jayellho@gmail.com>
2025-04-14 19:58:09 -0400

    edit: 1.1 is unique - minor cleanup.

--- 1_arrays-and-strings/1.1-is-unique.py
+++ 1_arrays-and-strings/1.1-is-unique.py
@@ -4,11 +4,6 @@ Implement an algorithm to determine if a string has all unique characters.
 What if you cannot use additional data structures?
 '''
 
-'''
-sabs
-abss
-'''
-
 def isUnique(s):
     n = len(s)
     if n <= 1:
@@ -22,12 +17,9 @@ def isUnique(s):
             return False
     return True
 
-
 # ==== Test cases ====
-
 s1 = ""
 assert True == isUnique(s1)
-
 
 s2 = "abababa"
 assert False == isUnique(s2)
