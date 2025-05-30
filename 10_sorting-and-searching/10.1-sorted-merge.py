809f3e7c58832298aa75426c25ccc9b86965c35f
jayellho <jayellho@gmail.com>
2025-05-30 14:59:22 -0400

    done: 10.1 sorted merge.

--- 10_sorting-and-searching/10.1-sorted-merge.py
+++ 10_sorting-and-searching/10.1-sorted-merge.py
@@ -10,14 +10,10 @@ Write a method to merge B into A in sorted order.
 - start from the end of both arrays, and decrease until 0. inserting from the back.
     - compare and pick the larger number than move the pointers.
 '''
-
-
 def sortedMerge(a, b):
-
     n = len(a)
     ptrB = len(b) - 1
     ptrA = n - len(b) - 1
-
 
     for i in range(n-1, -1, -1):
         # do comparison of items at pointers.
@@ -31,20 +27,30 @@ def sortedMerge(a, b):
         elif ptrA < 0:
             a[i] = b[ptrB]
             ptrB -= 1
-        else:
-            a[i] = a[ptrA]
-            ptrA -= 1
-    
     return a
 
 
-
-    
-
-
-
+# test case(s)
+# 1 - interleaved A and B.
 a = [2,4,5,7,9,11,0,0,0]
 b = [3,6,8]
+sortedL = sortedMerge(a, b)
+print(sortedL)
 
+# 2 - interleaved A and B.
+a = [2,4,5,7,9,11,0,0,0]
+b = [1,6,8]
+sortedL = sortedMerge(a, b)
+print(sortedL)
+
+# 3 - A after B.
+a = [2,5,7,9,12,0,0,0,0,0]
+b = [13,17,19,21,25]
+sortedL = sortedMerge(a, b)
+print(sortedL)
+
+# 4 - empty A
+a = [0, 0, 0, 0, 0]
+b = [1, 2, 3, 4, 5]
 sortedL = sortedMerge(a, b)
 print(sortedL)
