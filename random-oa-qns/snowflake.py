2ef5be58386032a34c8e6a7db8408b45763e0b66
jayellho <jayellho@gmail.com>
2025-10-16 17:23:27 -0400

    feat: snowflake OA quesation.

---
+++ random-oa-qns/snowflake.py
@@ -0,0 +1,4 @@
+# Given two strings, check if they match or not.
+# Strings can contain numbers which represent wildcards, and the number representing the number of wildcards.
+# e.g. sn4w, s5w ==> match
+#      s100k2, s75wwwww20kkk ==> match
